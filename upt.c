393723417ae1b57f8637f2a4b10017333ac79299
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-21 16:25:45 +0530

    Create upt.c

---
+++ upt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+int n,N,sum;
+
+printf("Enter value upto n number has to be calculated");
+scanf("%d",&n);
+N=n*n-n;
+sum=N/2;
+printf("sum of natural number is %d",sum);
+return 0;
+}
